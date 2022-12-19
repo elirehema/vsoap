2914fb6229d05b4d5809fef22058d8a56863846f
elirehema <rocksandystone@gmail.com>
2022-12-19 10:21:48 +0300

    Reload page if noot logged IN

--- store/authentication.js
+++ store/authentication.js
@@ -1,73 +1,77 @@
 const state = () => ({
   showLoader: Boolean,
-  accessToken: null
-})
+  accessToken: null,
+});
 
 const mutations = {
-  ['AUTHENTICATE'] (state) {
-    state.showLoader = true
+  ["AUTHENTICATE"](state) {
+    state.showLoader = true;
   },
-  ['AUTHENTICATE_FAILED'] (state) {
-    state.showLoader = false
+  ["AUTHENTICATE_FAILED"](state) {
+    state.showLoader = false;
   },
-  ['AUTHENTICATE_ERROR'] (state) {
-    state.showLoader = false
+  ["AUTHENTICATE_ERROR"](state) {
+    state.showLoader = false;
   },
-  ['AUTHENTICATE_SUCCESS'] (state, payload) {
-    state.showLoader = false
-    state.accessToken = payload.accessToken
-    localStorage.setItem('accessToken',
-      payload.accessToken
-    )
-    document.cookie = `current_subject=user::${payload.username}; SameSite=None; Secure`
-    //document.cookie = "favorite_food=tripe; SameSite=None; Secure";
+  ["AUTHENTICATE_SUCCESS"](state, payload) {
+    state.showLoader = false;
 
-    this.$router.push('/')
+    console.log(payload.accessToken)
+    if (payload.accessToken != undefined) {
+      state.accessToken = payload.accessToken;
+      localStorage.setItem("accessToken", payload.accessToken);
+      document.cookie = `current_subject=user::${payload.username}; SameSite=None; Secure`;
+    
+      this.$router.push("/");
+    }else{
+      window.location.reload(true)
+    }
   },
-  ['LOGOUT_SESSION'](state){
-    window.localStorage.clear()
-    sessionStorage.clear()
-    this.$router.push('/signin')
-  }
-}
+  ["LOGOUT_SESSION"](state) {
+    window.localStorage.clear();
+    sessionStorage.clear();
+    this.$router.push("/signin");
+    window.location.reload(true)
+  },
+};
 const actions = {
-  async _authenticate ({ commit, dispatch }, requestbody) {
-    commit('AUTHENTICATE')
+  async _authenticate({ commit, dispatch }, requestbody) {
+    commit("AUTHENTICATE");
     await this.$axios
-      .$post('/api/auth/signin', requestbody)
+      .$post("/api/auth/signin", requestbody)
       .then((response) => {
-        commit('AUTHENTICATE_SUCCESS', response)
+        commit("AUTHENTICATE_SUCCESS", response);
       })
       .catch(() => {
-        commit('AUTHENTICATE_ERROR')
-      })
+        commit("AUTHENTICATE_ERROR");
+      });
   },
-  async _createnewuser ({ dispatch }, payload) {
-    await this.$axios.$post('/api/users/register', payload)
+  async _createnewuser({ dispatch }, payload) {
+    await this.$axios
+      .$post("/api/users/register", payload)
       .then((response) => {
         dispatch("_fetchusers", null, { root: true });
-      }).catch((err) => {
       })
+      .catch((err) => {});
   },
 
-  async _logoutsession ({ commit }) {
-    await commit('LOGOUT_SESSION')
-  }
-}
+  async _logoutsession({ commit }) {
+    await commit("LOGOUT_SESSION");
+  },
+};
 const getters = {
   accessToken: function (state) {
-    return state.accessToken
+    return state.accessToken;
   },
   isAuthenticated: function (state) {
-    return state.accessToken != null
-  }
-
-}
+    return state.accessToken != null;
+  },
+};
 
 export default {
   namespaced: false,
   state,
   mutations,
   actions,
-  getters
-}
+  getters,
+};
